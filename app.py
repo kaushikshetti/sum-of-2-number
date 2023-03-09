117a16260979b4add395f41ed7be9995f59a7fc1
Kaushik Shetti <kshetti29@gmail.com>
2023-03-09 21:04:03 +0530

    final commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/sum', methods=['POST'])
+def calculate_sum():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    result = num1 + num2
+    return render_template('result.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run()
